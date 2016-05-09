9c32c82862a0ca6b40ec8e75a1733d4cf76d1f3e
gws0000 <stapletong@grantek.burlington>
2016-05-09 15:21:28 -0400

    Setup
    Initial Push

---
+++ ant.js
@@ -0,0 +1,207 @@
+const DEAD = 0;
+const ALIVE = 1;
+
+var ANT_Game = {
+	running: false,
+	c: null,
+	ctx: null,
+	gridPath: null,
+	cs: 20,
+	x0: 150,
+	y0: 100,
+	w: 600,
+	h: 400,
+	cells: [],
+	loops: 0,
+	timeFPS: 0,
+	countFPS: 0,
+	timeLast: 0,
+	maxAge: 30,
+	spawnChance: 0.3,
+	ants: [],
+
+	//Initializes the simulation
+	//	1)  Calculates some grid size parameters
+	//	2)  Creates a canvas/context for drawing
+	//  3)  Starts the animation cycle running
+	init: function() {
+		$("BODY").append($("<canvas id='myCanvas' width=800 height=600></canvas>"));
+		this.c = $("#myCanvas")[0];
+		$("#myCanvas").click(function(e) {
+			
+			//Location is set with e.offsetX and e.offsetY
+			//Is this a click in the grid?
+			if (e.offsetX > ANT_Game.x0 && e.offsetX < ANT_Game.x0+ANT_Game.w && e.offsetY > ANT_Game.y0 && e.offsetY < ANT_Game.y0+ANT_Game.h) {
+				var a = new DizzyAnt(e.offsetX, e.offsetY, 0, 10);
+				ANT_Game.ants.push(a);
+				
+				var a = new Ant(e.offsetX-50, e.offsetY-50, 0, 5);
+				ANT_Game.ants.push(a);
+				
+				var z = new ZiggyAnt(e.offsetX+50, e.offsetY+50, 0, 5);
+				ANT_Game.ants.push(z);
+				
+			}
+			
+		});
+		this.ctx = this.c.getContext("2d");
+		this.ctx.font = "12px Verdana";
+		this.ctx.fillStyle = "#CCCCCC";
+		this.ctx.fillRect(0,0,this.c.width, this.c.height);
+		
+		//this.cells.push(new Cell(10,10));
+		this.ctx.fillRect(0,0,800,600);
+		this.gridPath = this.setupGrid(this.x0,this.y0,this.w, this.h, this.cs);
+		ANT_Game.getFrame();
+	},
+	
+	//Builds a static path for the grid so redrawing is quick
+	setupGrid :	function (x0, y0, w, h, cs) {
+	var g = new Path2D();
+	
+		if (x0 > x1 || y0 > y1) throw("Invalid values");
+		var x1 = x0+w;
+		var y1 = y0 + h;
+	
+		var nx = w/cs;
+		var ny = h/cs;
+		
+		for (i=0;i<=nx;i++) {
+			g.moveTo(x0 + i*cs,y0);
+			g.lineTo(x0+i*cs, y1);
+		}
+
+		for (i=0;i<=ny;i++) {
+			g.moveTo(x0,y0+i*cs);
+			g.lineTo(x1,y0+i*cs);
+		}
+		
+		return g;
+	},
+	
+	//Builds an animation frame as required
+	getFrame : function() {
+		var d = new Date();
+		var m1 = d.getTime();
+		
+		//Clear the rectangle, and draw the base grid
+		ANT_Game.ctx.fillStyle = '#CCCCCC';
+		ANT_Game.ctx.fillRect(0,0,800,600);
+		//ANT_Game.ctx.stroke(ANT_Game.gridPath);
+		ANT_Game.ctx.textAlign = "center";
+		ANT_Game.ctx.textBaseline = "middle";
+
+		//Only change things if the simulation is "running"
+		if (ANT_Game.running) {
+			
+			//Move the ants
+			for (var i=0;i<ANT_Game.ants.length;i++) {
+				ANT_Game.ants[i].update(1/60);
+			}
+			
+		}
+		
+		//Draw the ants
+		for (var i=0;i<ANT_Game.ants.length;i++) {
+			ANT_Game.ants[i].draw(ANT_Game.ctx);
+		}
+
+	
+		//Sort cells by x, then by y, then by age.  This will speed checking for neighbours later on
+		ANT_Game.cells.sort(function(a,b) {
+			if (a.x < b.x) return -1;
+			if (a.x > b.x) return 1;
+			if (a.y < b.y) return -1;
+			if (a.y > b.y) return 1;
+			if (a.age < b.age) return -1;
+			return 1;
+		});
+		
+		//Update the cycle time
+		d = new Date();
+		var m2 = d.getTime();
+		
+		ANT_Game.ctx.textAlign = "left";
+		ANT_Game.ctx.textBaseline = "top";
+
+		ANT_Game.ctx.strokeStyle = "black";
+		ANT_Game.ctx.strokeText(ANT_Game.ants.length + " " + (m2-m1), 10,10);
+		window.requestAnimationFrame(ANT_Game.getFrame);
+	},
+	
+	//Keep cells inside the box
+	limitCell: function(c) {
+		if (c.x < 0) c.x = 0;
+		if (c.y < 0) c.y = 0;
+		if (c.x > ANT_Game.w/ANT_Game.cs-1) c.x = ANT_Game.w/ANT_Game.cs-1;
+		if (c.y > ANT_Game.h/ANT_Game.cs-1) c.y = ANT_Game.h/ANT_Game.cs-1;
+	}
+}
+
+$(document).ready(function() {ANT_Game.init();});
+
+function Cell(x,y) {
+	this.x = x;
+	this.y=y;
+	this.state = ALIVE;
+	this.age = 1;
+}
+
+//And ant that goes in a straight line
+function Ant(x,y,direction, speed) {
+		this.x=x;
+		this.y=y;
+		this.direction = direction;
+		this.speed = speed;
+		this.color = "black";
+}
+
+Ant.prototype.update = function(dt) {
+	this.x += Math.cos(this.direction) * this.speed * dt;
+	this.y += Math.sin(this.direction) * this.speed * dt;
+}
+
+Ant.prototype.draw = function(ctx) {
+	ctx.beginPath();
+	ctx.strokeStyle = this.color;
+	//ctx.moveTo(this.x, this.y);
+	ctx.arc(this.x, this.y, 3, 0, 2*Math.PI);
+	ctx.moveTo(this.x,this.y);
+	ctx.lineTo(this.x-Math.cos(this.direction)*10, this.y-Math.sin(this.direction)*10 );
+	ctx.stroke();
+}
+
+//An ant that goes in a circle
+DizzyAnt.prototype = new Ant();
+
+function DizzyAnt(x,y,direction,speed) {
+	Ant.apply(this, arguments);
+	this.color = "red";
+}
+
+DizzyAnt.prototype.update = function(dt) {
+	this.direction += Math.PI*dt;
+	this.x += Math.cos(this.direction) * this.speed * dt;
+	this.y += Math.sin(this.direction) * this.speed * dt;
+}
+
+//An any that zig/zags
+ZiggyAnt.prototype = new Ant();
+
+function ZiggyAnt(x,y,direction,speed) {
+	Ant.apply(this, arguments);
+	this.direction += Math.PI*45/180;
+	this.color = 'blue';
+	this.counter = 0;
+}
+
+ZiggyAnt.prototype.update = function(dt) {
+	if (this.counter++ == 60) {
+		this.direction -= Math.PI * 90 / 180;
+	} else if (this.counter == 120) {
+		this.counter = 0;
+		this.direction += Math.PI * 90 / 180;
+	}
+	this.x += Math.cos(this.direction) * this.speed * dt;
+	this.y += Math.sin(this.direction) * this.speed * dt;
+}
