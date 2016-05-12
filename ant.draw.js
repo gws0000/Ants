eacadc49168f721594ceaea8a59da12e22e08f27
gws0000 <stapletong@grantek.burlington>
2016-05-12 15:56:06 -0400

    Filling the ants head

--- ant.draw.js
+++ ant.draw.js
@@ -26,8 +26,10 @@ function drawDead(ctx) {
 	ctx.beginPath();
 	ctx.globalAlpha = this.opacity;
 	ctx.strokeStyle = this.color;
+	ctx.fillStyle = this.color;
 	//ctx.moveTo(this.x, this.y);
 	ctx.arc(this.x, this.y, 3, 0, 2*Math.PI);
+	ctx.fill();
 
 	ctx.moveTo(this.x-3, this.y-3);
 	ctx.lineTo(this.x+3, this.y+3);
